0984ebc45b6c0615ad6b5edcbb7dedd85a4d10d0
johncooked <152047905+y00do@users.noreply.github.com>
2025-07-21 18:09:49 +1000

    add scroll event listener to navbar for dynamic styling

--- src/app/main-page/sections/navbar/navbar.ts
+++ src/app/main-page/sections/navbar/navbar.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.scss'
+	selector: 'app-navbar',
+	imports: [],
+	templateUrl: './navbar.html',
+	styleUrl: './navbar.scss',
 })
 export class Navbar {
+	@HostBinding('class.scrolled') scrolled = false;
 
+	@HostListener('window:scroll', [])
+	onWindowScroll() {
+		this.scrolled = window.scrollY > 0;
+	}
 }
