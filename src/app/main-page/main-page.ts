52f5814e937c5e1c37baa09193c994a2786333c3
johncooked <152047905+y00do@users.noreply.github.com>
2025-07-26 21:48:26 +1000

    add Hero component to main-page imports

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from './sections/navbar/navbar';
+import { Hero } from './sections/hero/hero';
 
 @Component({
-  selector: 'main-page',
-  standalone: true,
-  imports: [Navbar],
-  templateUrl: './main-page.html',
-  styleUrl: './main-page.scss',
+	selector: 'main-page',
+	standalone: true,
+	imports: [Navbar, Hero],
+	templateUrl: './main-page.html',
+	styleUrl: './main-page.scss',
 })
 export class MainPage {}
